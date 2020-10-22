8e64a71674779844ebfa4bcfaf7dd962decfde0c
kush16816 <kushagravi13@gmail.com>
2020-10-22 23:45:35 +0530

    Added test cases for enhanced invoice

--- src/test/java/com/capgemini/cabservice/AppTest.java
+++ src/test/java/com/capgemini/cabservice/AppTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.capgemini.cabservice.service.EnhancedInvoice;
 import com.capgemini.cabservice.service.GenerateInvoice;
 import com.capgemini.cabservice.service.RideDetails;
 
@@ -33,5 +34,44 @@ public class AppTest {
 		rideList.add(new RideDetails(8, 10));
 		double totalFare = GenerateInvoice.aggregateTotalFare(rideList);
 		assertEquals(339.0, totalFare, 0);
+	}
+
+	@Test
+	public void testForFareOfEnhancedInvoice() {
+		List<RideDetails> rideList = new ArrayList<RideDetails>();
+		rideList.add(new RideDetails(0.6, 4));
+		rideList.add(new RideDetails(7, 16));
+		rideList.add(new RideDetails(12, 25));
+		rideList.add(new RideDetails(0.7, 6));
+		rideList.add(new RideDetails(8, 10));
+		EnhancedInvoice invoice = GenerateInvoice.getEnhacedInvoice(rideList);
+		double totalFare = invoice.getTotalFare();
+		assertEquals(339.0, totalFare, 0);
+	}
+
+	@Test
+	public void testForNoOfRidesOfEnhancedInvoice() {
+		List<RideDetails> rideList = new ArrayList<RideDetails>();
+		rideList.add(new RideDetails(0.6, 4));
+		rideList.add(new RideDetails(7, 16));
+		rideList.add(new RideDetails(12, 25));
+		rideList.add(new RideDetails(0.7, 6));
+		rideList.add(new RideDetails(8, 10));
+		EnhancedInvoice invoice = GenerateInvoice.getEnhacedInvoice(rideList);
+		int noOfRides = invoice.getNoOfRides();
+		assertEquals(5, noOfRides, 0);
+	}
+
+	@Test
+	public void testForAverageFareOfEnhancedInvoice() {
+		List<RideDetails> rideList = new ArrayList<RideDetails>();
+		rideList.add(new RideDetails(0.6, 4));
+		rideList.add(new RideDetails(7, 16));
+		rideList.add(new RideDetails(12, 25));
+		rideList.add(new RideDetails(0.7, 6));
+		rideList.add(new RideDetails(8, 10));
+		EnhancedInvoice invoice = GenerateInvoice.getEnhacedInvoice(rideList);
+		double averageFare = invoice.getAverageFare();
+		assertEquals(67.8, averageFare, 0);
 	}
 }
